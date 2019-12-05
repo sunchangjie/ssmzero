4cb8dbf58b9076ca60bbacd5cce942c05dcc004a
sunchangjie <changjie.sun@hand-china.com>
2019-12-05 12:44:41 +0800

    update...

--- src/main/java/com/hand/ssm/controller/UserCacheController.java
+++ src/main/java/com/hand/ssm/controller/UserCacheController.java
@@ -7,6 +7,7 @@ import org.springframework.cache.CacheManager;
 import org.springframework.cache.ehcache.EhCacheCacheManager;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.hand.ssm.domain.User;
 import com.hand.ssm.service.IUserService;
@@ -20,34 +21,54 @@ public class UserCacheController {
     private IUserService userService;
 
     @RequestMapping(value = "/getUsersByNameAndAge.do")
-    public User getUsersByNameAndAge() {
-        return userService.getUsersByNameAndAge("scj", 18);
+    public ModelAndView getUsersByNameAndAge() {
+        ModelAndView mv = new ModelAndView();
+        User user = userService.getUsersByNameAndAge("scj", 18);
+        mv.addObject("result", user);
+        mv.setViewName("displayresult");
+        return mv;
     }
 
 
     @RequestMapping(value = "/getAnotherUser.do")
-    public User getAnotherUser(String name, int age) {
-        return userService.getAnotherUser("scj", 18);
+    public ModelAndView getAnotherUser(String name, int age) {
+        ModelAndView mv = new ModelAndView();
+        User user = userService.getAnotherUser("scj", 18);
+        mv.addObject("result", user);
+        mv.setViewName("displayresult");
+        return mv;
     }
 
 
     @RequestMapping(value = "/evictUser.do")
-    public void evictUser(String name, int age) {
+    public ModelAndView evictUser(String name, int age) {
+        ModelAndView mv = new ModelAndView();
         userService.evictUser("scj", 18);
+        mv.addObject("result", "清除完成evictUser");
+        mv.setViewName("displayresult");
+        return mv;
     }
 
     @RequestMapping(value = "/evictAll.do")
-    public void evictAll() {
+    public ModelAndView evictAll() {
+        ModelAndView mv = new ModelAndView();
         userService.evictAll();
+        mv.addObject("result", "清除完成evictAll");
+        mv.setViewName("displayresult");
+        return mv;
     }
 
     private CacheManager manager;
 
     @RequestMapping(value = "/getUserCache.do")
-    public void getUserCache() {
-        User user =  (User)manager.getCache("users");
+    public ModelAndView getUserCache() {
+        ModelAndView mv = new ModelAndView();
+        User user = (User) manager.getCache("users");
         System.out.println(user);
         userService.evictAll();
+        mv.addObject("result", user);
+        mv.setViewName("displayresult");
+        return mv;
     }
 
 }
---
+++ src/main/webapp/pages/displayresult.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: cjs
+  Date: 2019/12/5
+  Time: 12:30 下午
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ taglib prefix="c"
+           uri="http://java.sun.com/jsp/jstl/xml" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+结果：${result}
+</body>
+</html>
